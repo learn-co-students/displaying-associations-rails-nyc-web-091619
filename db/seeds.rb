77ea21458e5b55cbae40f2d5df8a583d66dd5645
Danny Lee <dreamer@panix.com>
2019-10-15 22:58:21 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'destroying old data'
+Post.destroy_all
+Category.destroy_all
+
+puts "seeding database"
+puts 'creating posts'
+clickbait = Category.create!(name: "Motivation")
+clickbait.posts.create!(title: "10 Ways You Are Already Awesome")
+clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
+puts 'adding movie'
+movies = Category.create!(name: "Movies")
+movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
+puts 'done!'
